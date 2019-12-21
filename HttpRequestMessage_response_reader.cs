59768c25640411ec438a0d282443985158759022
GowthamThangamani <vtgowtham1993@gmail.com>
2019-12-21 13:46:08 +0530

    Update HttpRequestMessage_response_reader.cs

--- HttpRequestMessage_response_reader.cs
+++ HttpRequestMessage_response_reader.cs
@@ -81,6 +81,35 @@ namespace System.Web.Http
 
             return null;
         }
+        
+        //To Read file from http request which is send as Multipart form data
+        // Mime file send
+         public static async Task<HttpPostedData> ParseMultipartAsync( HttpContent postedContent)
+        {
+            var provider = await postedContent.ReadAsMultipartAsync();
+
+            var files = new Dictionary<string, HttpPostedFile>(StringComparer.InvariantCultureIgnoreCase);
+            var fields = new Dictionary<string, HttpPostedField>(StringComparer.InvariantCultureIgnoreCase);
+
+            foreach (var content in provider.Contents)
+            {
+                var fieldName = content.Headers.ContentDisposition.Name.Trim('"');
+                if (!string.IsNullOrEmpty(content.Headers.ContentDisposition.FileName))
+                {
+                    var file = await content.ReadAsByteArrayAsync();
+                    var fileName = content.Headers.ContentDisposition.FileName.Trim('"');
+                    files.Add(fieldName, new HttpPostedFile(fieldName, fileName, file));
+                }
+                else
+                {
+                    var data = await content.ReadAsStringAsync();
+                    fields.Add(fieldName, new HttpPostedField(fieldName, data));
+                }
+            }
+
+            return new HttpPostedData(fields, files);
+        }
+    }
 
     }
 }
